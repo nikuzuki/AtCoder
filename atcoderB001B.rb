d2b6d2c49df2d4525558d681e976c7274606aa03
Kohei Makita <nikuzuki29@gmail.com>
2015-06-23 02:03:33 +0900

    change p -> puts

--- atcoderB001B.rb
+++ atcoderB001B.rb
@@ -3,18 +3,18 @@ m = STDIN.gets.to_f
 m = m / 1000
 
 if m < 0.1 then
-	p vv = "00"
+	puts "00"
 elsif m >= 0.1 && m <= 5.0 then
-	p ten = m.to_i
-	p one = (m - ten.to_i) * 10
-	p vv = ten.to_i.to_s + one.to_i.to_s
+	puts ten = m.to_i
+	puts one = (m - ten.to_i) * 10
+	puts vv = ten.to_i.to_s + one.to_i.to_s
 	#ten.to_i //100 -> 0
 elsif m >= 6.0 && m <= 30.0 then
 	m = m + 50
-	p vv = m.to_i.to_s
+	puts vv = m.to_i.to_s
 elsif m >= 35.0 && m <= 70.0 then
 	m = (m - 30) / 5.0 + 80
-	p vv = m.to_i.to_s
+	puts vv = m.to_i.to_s
 elsif m > 70.0
-	p vv = "89"
+	puts "89"
 end
