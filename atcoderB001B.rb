6ec67af663a6c8385133c9abb1fc7ba29ca829d1
Kohei Makita <nikuzuki29@gmail.com>
2015-06-24 01:35:09 +0900

    float bug fixed

--- atcoderB001B.rb
+++ atcoderB001B.rb
@@ -1,20 +1,23 @@
 m = STDIN.gets.to_f
 
-m = m / 1000
+m = m / 1000.0
 
 if m < 0.1 then
 	puts "00"
 elsif m >= 0.1 && m <= 5.0 then
 	ten = m.to_i
-	one = (m - ten.to_i) * 10
-	puts vv = ten.to_i.to_s + one.to_i.to_s
+	one = (m - ten.to_i) * 10.0
+	vv = ten.to_i.to_s + one.to_i.to_s
+	puts vv
 	#ten.to_i //100 -> 0
 elsif m >= 6.0 && m <= 30.0 then
-	m = m + 50
-	puts vv = m.to_i.to_s
+	m = m + 50.0
+	vv = m.to_i.to_s
+	puts vv
 elsif m >= 35.0 && m <= 70.0 then
-	m = (m - 30) / 5.0 + 80
-	puts vv = m.to_i.to_s
+	m = (m - 30.0) / 5.0 + 80
+	vv = m.to_i.to_s
+	puts vv
 elsif m > 70.0
 	puts "89"
 end
